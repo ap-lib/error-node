117f4474b29805a6b6d3df219ae1232959fe9908
Anton Panfilov <anton@panfilov.biz>
2025-03-03 18:26:24 -0800

    error context

--- src/Error.php
+++ src/Error.php
@@ -4,10 +4,22 @@ namespace AP\ErrorNode;
 
 class Error
 {
+    readonly public string $message;
+
     public function __construct(
-        readonly public string $message,
-        public array           $path = [],
+        string                $message,
+        public array          $path = [],
+        readonly public array $context = [],
     )
     {
+        if (empty($this->context)) {
+            $this->message = $message;
+        } else {
+            $keys = [];
+            foreach ($this->context as $k => $v) {
+                $keys[] = '{' . $k . '}';
+            }
+            $this->message = str_replace($keys, $this->context, $message);
+        }
     }
 }
